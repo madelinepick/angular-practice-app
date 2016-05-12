1f837ccd7740a04b96c3d86fa21a981a81f23a35
Madeline Pickering <madelinepickering@gmail.com>
2016-05-12 14:28:45 -0600

    sign up works

---
+++ public/javascripts/signup.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .controller('SignupController', function ($scope, $http, $window, $location) {
+      $scope.newUser = function(){
+        $http.post('/api/v1/users/signup', $scope.user).then(function (response) {
+          $window.localStorage.setItem('token', response.data.token);
+          $location.path('/');
+        })
+      }
+    })
+
+}());
